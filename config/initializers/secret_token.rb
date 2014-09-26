4f5e90d7685e0cbbbd78332a167ae101bedeaa76
Dexter Codell <dextercodell@ts-MacBook-Pro.local>
2014-09-26 11:22:34 -0500

    This is teh first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DexCodes::Application.config.secret_key_base = '0ad35b69d4e8aa3defcc2bee630de66151d6559676d82f991ebdfa578ef7aaa362717b2981c0c00f11c465645642e1e89535afa8c59f56ae3b47ce7b6c269526'
